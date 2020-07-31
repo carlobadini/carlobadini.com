0186f90432f10f4d9f5cc267fd074152c38b1c28
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-31 17:03:48 +0530

    :fire: Remove blog routes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,60 +1,50 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
-</nav>
+<header>
+  <h1>Carlo Badini</h1>
+  <p>
+    Entrepreneur, Early Stage Investor. likes tech, design, economics,
+    psychology, chestnuts
+  </p>
+  <nav>
+    <ul>
+      <li>
+        <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+          Intro
+        </a>
+      </li>
+      <li>
+        <a aria-current={segment === 'work' ? 'page' : undefined} href="work">
+          Work
+        </a>
+      </li>
+      <li>
+        <a
+          aria-current={segment === 'learning' ? 'page' : undefined}
+          href="learning">
+          Learning
+        </a>
+      </li>
+      <li>
+        <a
+          aria-current={segment === 'investments' ? 'page' : undefined}
+          href="investments">
+          Investments
+        </a>
+      </li>
+      <li>
+        <a
+          aria-current={segment === 'body-mind' ? 'page' : undefined}
+          href="body-mind">
+          Body &amp; Mind
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
