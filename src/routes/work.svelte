9f8e53d79514dcf2befbef325ed8773ce47df20b
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-06 16:55:43 +0530

    :recycle: Add logos and other works

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -11,6 +11,11 @@
   blockquote {
     margin: 0;
   }
+  ul + h2 {
+    border-top: 1px solid #dedede;
+    margin-top: 2rem;
+    padding-top: 2rem;
+  }
 </style>
 
 <svelte:head>
@@ -22,7 +27,7 @@
   <blockquote>
     <p>
       “I’m a greater believer in luck, and I find the harder I work the more I
-      have of it.” - Thomas Jefferson
+      have of it.” – Thomas Jefferson
     </p>
   </blockquote>
   <h2>Experiences 👨‍💻</h2>
@@ -35,7 +40,7 @@
       </div>
       <div>
         <div>Founder/CEO Koj AG</div>
-        <div>2020 - today</div>
+        <div>2020–today</div>
         <div>
           Koj is an interior design subscription service. With Koj you get your
           apartment fully furnished by a professional interior designer and rent
@@ -51,7 +56,7 @@
       </div>
       <div>
         <div>Founder/Owner Cleverclip GmbH</div>
-        <div>2013 - today</div>
+        <div>2013–today</div>
         <div>
           Cleverclip is a design and communication agency that produces
           explainer videos and interactive experiences for customers such as:
@@ -67,7 +72,7 @@
       </div>
       <div>
         <div>Board Member Comvation AG</div>
-        <div>2019 - today</div>
+        <div>2019–today</div>
         <div>
           Comvation is a well established web agency from Thun, Switzerland.
           Comvation develops websites, web applications and online marketing
@@ -83,11 +88,175 @@
       </div>
       <div>
         <div>Board Member Daktiv AG</div>
-        <div>2016 - today</div>
+        <div>2016–today</div>
         <div>
           Daktiv is a live communication agency from Bern, Switzerland. Daktiv
           develops roadshows, exhibitions and live events for customers such as
           SUVA, Coop, Volvo and Swatch.
+        </div>
+      </div>
+    </li>
+  </ul>
+  <h2>Awards and Recognition 🏆</h2>
+  <ul class="list">
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/ey.png" />
+        </div>
+      </div>
+      <div>
+        <div>Ernst & Young Entrepreneur of the Year – Nominated</div>
+        <div>2018</div>
+        <div>
+          EY Entrepreneur Of The Year is a global program that recognises
+          entrepreneurial achievement among individuals and companies that
+          demonstrate vision, leadership and success.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/forbes.svg" />
+        </div>
+      </div>
+      <div>
+        <div>Forbes Magazine – 30 Under 30</div>
+        <div>2018</div>
+        <div>
+          With 30 Under 30 Forbes features every year the brightest young
+          entrepreneurs, leaders, and stars worldwide.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/sef.jpg" />
+        </div>
+      </div>
+      <div>
+        <div>Swiss Economic Award – Finalist</div>
+        <div>2017</div>
+        <div>
+          The Swiss Economic Award is the most prestigious startup award in
+          Switzerland and recognises outstanding achievements in business.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/cwt.png" />
+        </div>
+      </div>
+      <div>
+        <div>Bernese Youth Award – 1st prize</div>
+        <div>2015</div>
+        <div>
+          Since 1995 the Burger community of Bern awards the Bernese Youth Award
+          for outstanding young talents.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/gsea.png" />
+        </div>
+      </div>
+      <div>
+        <div>
+          Global Student Entrepreneur Awards – 1st place European champion
+        </div>
+        <div>2015</div>
+        <div>
+          The Global Student Entrepreneur Awards (GSEA) is the premier global
+          competition for students who own and operate a for-profit business
+          while attending college or university.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/ssa.png" />
+        </div>
+      </div>
+      <div>
+        <div>Swiss Startups Awards – Public’s Choice Award</div>
+        <div>2013</div>
+        <div>
+          Cleverclip received the Swiss Startups Awards, Public’s Choice Award
+          for the best business idea of 2013.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/gk.png" />
+        </div>
+      </div>
+      <div>
+        <div>Stauffacherpreis – 1st place</div>
+        <div>2011</div>
+        <div>
+          The “Stauffacherpreis” is a yearly award handed out by the Gymnasium
+          Kirchenfeld for the best final paper among all graduates.
+        </div>
+      </div>
+    </li>
+  </ul>
+  <h2>Speaking Engagements 🎤</h2>
+  <p>
+    From time to time I get invited to talk at universities, forums and events
+    about the things I love. Here’s an incomplete list of places I talked about
+    startups, leadership, remote work, storytelling and digital marketing.
+    Inquiries can be made to:
+    <a href="mailto:talks@carlobadini.com">talks@carlobadini.com</a>
+  </p>
+  <h2>Press Coverage 📺</h2>
+  <p>
+    The work of my team and me get’s occasionally mentioned in the press. The
+    following news outlets reported about us. Press inquiries can be made to:
+    <a href="mailto:media@carlobadini.com">media@carlobadini.com</a>
+  </p>
+  <h2>Volunteer Work 💖</h2>
+  <ul class="list">
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/daktiv.png" />
+        </div>
+      </div>
+      <div>
+        <div>Entrepreneurs Organisation – GSEA committee</div>
+        <div>2017–today</div>
+        <div>
+          The Global Student Entrepreneur Awards (GSEA) is the premier global
+          competition for students who own and operate a for-profit business
+          while attending college or university. Since 2017 I have served on the
+          committee in Switzerland and helped plan and organise the yearly
+          award.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/daktiv.png" />
+        </div>
+      </div>
+      <div>
+        <div>World Economic Forum - Global Shapers</div>
+        <div>2015–today</div>
+        <div>
+          The Global Shapers Community is an initiative from the World Economic
+          Forum. The GS are a network of young people driving dialogue, action
+          and change. As part of the member’s office I’m responsible for
+          recruiting and selecting outstanding people to join our community.
         </div>
       </div>
     </li>
