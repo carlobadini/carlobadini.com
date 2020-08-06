43abb37441bca830f4bde323e7b9ed07a2633250
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-06 16:26:49 +0530

    :lipstick: Add list style for experiences

---
+++ src/routes/work.svelte
@@ -0,0 +1,59 @@
+<style>
+  h1 {
+    margin-top: 0;
+  }
+  h2 {
+    margin-bottom: 1.5rem;
+  }
+  .text {
+    max-width: 700px;
+  }
+  blockquote {
+    margin: 0;
+  }
+</style>
+
+<svelte:head>
+  <title>Work · Carlo Badini</title>
+</svelte:head>
+
+<div class="text">
+  <h1>Work 💻</h1>
+  <blockquote>
+    <p>
+      “I’m a greater believer in luck, and I find the harder I work the more I
+      have of it.” - Thomas Jefferson
+    </p>
+  </blockquote>
+  <h2>Experiences 👨‍💻</h2>
+  <ul class="list">
+    <li>
+      <div>
+        <div class="img">1</div>
+      </div>
+      <div>
+        <div>Founder/CEO Koj AG</div>
+        <div>2020 - today</div>
+        <div>
+          Koj is an interior design subscription service. With Koj you get your
+          apartment fully furnished by a professional interior designer and rent
+          high quality furniture on a monthly subscription.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">1</div>
+      </div>
+      <div>
+        <div>Founder/CEO Koj AG</div>
+        <div>2020 - today</div>
+        <div>
+          Koj is an interior design subscription service. With Koj you get your
+          apartment fully furnished by a professional interior designer and rent
+          high quality furniture on a monthly subscription.
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
