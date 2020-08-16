2e7c581ec6f78f07106838bb1aa89a87102e7e1e
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-16 20:41:49 +0530

    :construction: Add investments page

---
+++ src/routes/investments.svelte
@@ -0,0 +1,106 @@
+<style>
+  h1 {
+    margin-top: 0;
+  }
+  .text {
+    max-width: 700px;
+  }
+  .list li div:last-child div:nth-child(2) {
+    opacity: 1;
+    margin-bottom: 0;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+</style>
+
+<svelte:head>
+  <title>Investments · Carlo Badini</title>
+</svelte:head>
+
+<div class="text">
+  <h1>Investments 💸</h1>
+  <p>
+    I invest mostly in early stage startups. Ticket sizes vary. If you’re part
+    of an outstanding startup and you’re looking for investors send me a message
+    to
+    <a href="mailto:invest@carlobadini.com">invest@carlobadini.com</a>
+  </p>
+  <ul class="list">
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/koj.svg" />
+        </div>
+      </div>
+      <div>
+        <div>Blink AG</div>
+        <div>
+          Blink is the first digital driving school in Switzerland. Blink
+          supports aspiring drivers from the first lesson until they get their
+          license.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/koj.svg" />
+        </div>
+      </div>
+      <div>
+        <div>Kingfluencers AG</div>
+        <div>
+          Kingfluencers is the leading influencer marketing platform in
+          Switzerland. Kingfluencers connects companies with influencers to
+          expand their brand recognition and reach new audiences.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/koj.svg" />
+        </div>
+      </div>
+      <div>
+        <div>RetinAI AG</div>
+        <div>
+          Retinai develops products to make research and health institutions
+          more efficient using latest technologies in machine learning, computer
+          vision and signal processing.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/koj.svg" />
+        </div>
+      </div>
+      <div>
+        <div>legal-i AG</div>
+        <div>
+          legal-i is an artificial intelligent attorney-at-law. With machine
+          learning legal-i supports lawyers in the field of medical insurance
+          law.
+        </div>
+      </div>
+    </li>
+    <li>
+      <div>
+        <div class="img">
+          <img alt="" src="/logos/koj.svg" />
+        </div>
+      </div>
+      <div>
+        <div>Daktiv AG (acquired)</div>
+        <div>
+          Daktiv is a live communication agency from Bern, Switzerland. Daktiv
+          develops roadshows, exhibitions and live events for customers such as
+          SUVA, Coop, Volvo and Swatch.
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
