0f8dc939b077e92e914e6df10fb4b8c9b67c3001
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-24 00:19:20 +0530

    :construction: Add body/mind page

---
+++ src/routes/body-mind.svelte
@@ -0,0 +1,26 @@
+<script>
+  import t from "../get-data";
+</script>
+
+<style>
+  h1 {
+    margin-top: 0;
+  }
+  .text {
+    max-width: 700px;
+  }
+</style>
+
+<svelte:head>
+  <title>{t('Text', 'body.title')} · {t('Text', 'name')}</title>
+</svelte:head>
+
+<div class="text">
+  <h1>{t('Text', 'body.title')}</h1>
+  <p>
+    {@html t('Text', 'body.subtitle')}
+  </p>
+  <!-- <h2>{$t('Text', 'body.productivity.title')}</h2> -->
+  <!-- <p>{$t('Text', 'body.productivity.subtitle')}</p>
+  <p>{$t('Text', 'body.productivity.content')}</p> -->
+</div>
