dced5c76b2f5010f629f684072e7d269e2095b6b
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-24 00:25:34 +0530

    :recycle: Don't use ?., use || {}

--- src/get-data.js
+++ src/get-data.js
@@ -4,9 +4,13 @@ const snarkdown = require("snarkdown");
 module.exports = (table, term) => {
   if (term)
     return snarkdown(
-      data[table][0].find((row) => {
-        return row.fields?.key === term;
-      })?.fields?.Content ?? ""
+      (
+        (
+          data[table][0].find((row) => {
+            return (row.fields || {}).key === term;
+          }) || {}
+        ).fields || {}
+      ).Content ?? ""
     ).replace(/<br \/>/g, "<br><br>");
   return data[table][0] ?? "";
 };
