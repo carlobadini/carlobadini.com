289fe07996e552f111d82fcc8f246cb6766b32f0
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-21 20:58:55 +0530

    :bug: Fix data order

--- src/get-data.js
+++ src/get-data.js
@@ -12,5 +12,12 @@ module.exports = (table, term) => {
         ).fields || {}
       ).Content ?? ""
     ).replace(/<br \/>/g, "<br><br>");
+  if (Array.isArray(data[table][0]))
+    return data[table][0].sort((a, b) => {
+      if (a.fields && a.fields.Order) {
+        return parseInt(a.fields.Order) - parseInt(b.fields.Order);
+      }
+      return 0;
+    });
   return data[table][0] ?? "";
 };
