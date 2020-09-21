53bc13150af24134613623b4ab9d8e74d77a616b
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-21 20:56:36 +0530

    :lipstick: Make homepage single column

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,23 +13,11 @@
   h1 {
     margin-top: 0;
   }
-  .f {
-    display: flex;
-  }
   img {
     margin: 0 auto;
-    margin-right: 2.5rem;
-    max-width: 350px;
-    display: block;
-  }
-  @media (max-width: 900px) {
-    img {
-      margin: 0 auto 2.5rem;
-      max-width: 100%;
-    }
-    .f {
-      display: block;
-    }
+    width: 100%;
+    margin-top: 2rem;
+    max-width: 700px;
   }
 </style>
 
@@ -37,22 +25,21 @@
   <title>{t('Text', 'name')}</title>
 </svelte:head>
 
-<div class="f">
-  <div>
-    <img
-      alt=""
-      src="https://dl.airtable.com/.attachmentThumbnails/cd676b6954c3ff979c254e9fe8c3ea9c/9c9ead7c" />
-  </div>
-  <div>
-    <h1>
-      {@html t('Text', 'about.title')}
-    </h1>
-    <p>
-      {@html t('Text', 'about.subtitle')}
-    </p>
-    <hr />
-    <p>
-      {@html t('Text', 'about.content')}
-    </p>
-  </div>
+<div>
+  <h1>
+    {@html t('Text', 'about.title')}
+  </h1>
+  <p>
+    {@html t('Text', 'about.subtitle')}
+  </p>
+  <hr />
+  <p>
+    {@html t('Text', 'about.content')}
+  </p>
+</div>
+
+<div>
+  <img
+    alt=""
+    src="https://dl.airtable.com/.attachmentThumbnails/cd676b6954c3ff979c254e9fe8c3ea9c/9c9ead7c" />
 </div>
